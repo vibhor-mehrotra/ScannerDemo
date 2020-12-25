f6d5b9c4cf33d690d2418a7af971a6424b3e210c
vibhor1523 <vib.meh@gmail.com>
2020-12-25 17:05:11 +0530

    Initial Commit

---
+++ ScannerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScannerDemo
+//
+//  Created by Vibhor Mehrotra on 25/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
